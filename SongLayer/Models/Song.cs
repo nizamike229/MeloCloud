cb05568e75d0792f277d4dbb46c676c7a03dac6f
Nizami <nizamike229@mail.ru>
2024-10-24 15:26:55 +0500

    Add models for SongLayer

---
+++ SongLayer/Models/Song.cs
@@ -0,0 +1,14 @@
+﻿namespace MyMusicApp.Model;
+
+public partial class Song
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Artists { get; set; } = null!;
+
+    public string SongPath{ get; set; } = null!;
+
+    public string CoverEncoded { get; set; } = null!;
+}
---
+++ SongLayer/Models/SongCreateModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MyMusicApp.Model;
+
+public class SongCreateModel
+{
+    public required string Name { get; set; } = null!;
+
+    public required string Artists { get; set; } = null!;
+
+    public required string SongEncoded { get; set; } = null!;
+    
+    public required string CoverEncoded { get; set; } = null!;
+}
