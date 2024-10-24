d43a752c388598f91d1d3322a2d81491dbbf7453
Nizami <nizamike229@mail.ru>
2024-10-24 15:53:36 +0500

    Add DBContext

---
+++ AuthLayer/AuthLayerContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using AuthLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthLayer;
+
+public partial class AuthLayerContext : DbContext
+{
+    public AuthLayerContext()
+    {
+    }
+
+    public AuthLayerContext(DbContextOptions<AuthLayerContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=auth_layer;Username=postgres;Password=password");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("users_pk");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Password)
+                .HasMaxLength(350)
+                .HasColumnName("password");
+            entity.Property(e => e.Username)
+                .HasMaxLength(255)
+                .HasColumnName("username");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
