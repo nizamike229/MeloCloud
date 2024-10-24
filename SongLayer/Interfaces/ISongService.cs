cbc734d864a3249020f06f6b9af09014a16dd02f
Nizami <nizamike229@mail.ru>
2024-10-24 15:26:37 +0500

    Add ISongService to be implemented

---
+++ SongLayer/Interfaces/ISongService.cs
@@ -0,0 +1,12 @@
+﻿using MyMusicApp.Model;
+
+namespace MyMusicApp.Interfaces;
+
+public interface ISongService
+{
+    Task AddSongAsync(SongCreateModel song);
+    Task<string> UpdateSongAsync(Song song);
+    Task DeleteSongAsync(int id);
+    Task<List<Song>> GetSongsAsync();
+    Task<Song> GetSongById(int id);
+}
