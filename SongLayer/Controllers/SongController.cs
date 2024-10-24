1b0ffe4d5f9bdcfdad262155b485d9097495c473
Nizami <nizamike229@mail.ru>
2024-10-24 15:29:35 +0500

    Add controllers but still don't implemented all service methods ( will)

---
+++ SongLayer/Controllers/SongController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyMusicApp.Interfaces;
+using MyMusicApp.Model;
+
+namespace MyMusicApp.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]/[action]")]
+public class SongController : ControllerBase
+{
+    private readonly ISongService _songService;
+
+    public SongController(ISongService songService)
+    {
+        _songService = songService;
+    }
+
+    [HttpGet]
+    [ActionName("all")]
+    public async Task<ActionResult<List<Song>>> GetSongsAsync()
+    {
+        var result = await _songService.GetSongsAsync();
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ActionName("create")]
+    public async Task<ActionResult> CreateSongAsync(SongCreateModel request)
+    {
+        await _songService.AddSongAsync(request);
+        return Ok("Song created successfully!");
+    }
+}
