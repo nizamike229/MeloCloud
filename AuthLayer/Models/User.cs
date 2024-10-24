ac34a71478993d68dde49b7a3624aa6c39ae6d45
Nizami <nizamike229@mail.ru>
2024-10-24 15:52:53 +0500

    Add models

---
+++ AuthLayer/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthLayer.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
---
+++ AuthLayer/Models/UserLoginRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthLayer.Models;
+
+public class UserLoginRequest
+{
+    [MinLength(2)]
+    public required string Username { get; set; }
+    
+    [MinLength(8)]
+    public required string Password { get; set; }
+}
