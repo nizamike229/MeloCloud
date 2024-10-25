8164db942d2ba03eba097414e532f9020592a181
Nizami <nizamike229@mail.ru>
2024-10-25 17:48:15 +0500

    Implemented methods

--- AuthLayer/Services/AuthService.cs
+++ AuthLayer/Services/AuthService.cs
@@ -34,6 +34,7 @@ public class AuthService : IAuthService
 
         var user = new User()
         {
+            Logo = "https://i.pinimg.com/736x/0c/da/40/0cda4058d21f8101ffcc223eec55c18f.jpg",
             Id = Guid.NewGuid(),
             Username = request.Username,
             Password = HashPassword(request.Password)
@@ -41,6 +42,38 @@ public class AuthService : IAuthService
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
         return user;
+    }
+
+    public async Task Edit(EditModel request)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == Guid.Parse(request.Id!));
+        if (request.Username != null)
+        {
+            var isUsernameFree = await _context.Users.FirstOrDefaultAsync(u => u.Username == request.Username) == null;
+            user!.Username = isUsernameFree ? request.Username : throw new Exception("Username already taken");
+        }
+
+        if (request.Logo != null)
+            user!.Logo = request.Logo;
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<UserResponse> GetPersonalInfoAsync(string id)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == Guid.Parse(id));
+
+        return user == null ? throw new Exception("Invalid user id") : new UserResponse
+        {
+            Username = user.Username,
+            Logo = user.Logo
+        };
+    }
+
+    public async Task<string> GetUsernameByIdAsync(string id)
+    {
+        var result= (await _context.Users.FirstOrDefaultAsync(u => u.Id == Guid.Parse(id)))!.Username;
+        return result;
     }
 
     private string HashPassword(string password)
