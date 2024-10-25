3444d6f8d42d22709b2f85258d39e9f1ec4f6bd6
Nizami <nizamike229@mail.ru>
2024-10-25 17:47:19 +0500

    Add global try-catch

---
+++ SongLayer/Middleware/ErrorHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace MyMusicApp.Middleware;
+
+public class ErrorHandler
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ErrorHandler> _logger;
+
+    public ErrorHandler(RequestDelegate next, ILogger<ErrorHandler> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message, "Произошла ошибка при обработке запроса.");
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Response.ContentType = "text/plain";
+            await context.Response.WriteAsync($"Произошла ошибка: {ex.Message}.");
+        }
+    }
+}
