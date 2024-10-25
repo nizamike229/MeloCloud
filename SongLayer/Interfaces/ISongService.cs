0ac906c9485b641dde782d03668233a9788ea7a4
Nizami <nizamike229@mail.ru>
2024-10-25 17:50:33 +0500

    Changed method parameter

--- SongLayer/Interfaces/ISongService.cs
+++ SongLayer/Interfaces/ISongService.cs
@@ -1,11 +1,11 @@
-﻿using MyMusicApp.Model;
+﻿using MyMusicApp.Models;
 
 namespace MyMusicApp.Interfaces;
 
 public interface ISongService
 {
     Task AddSongAsync(SongCreateModel song);
-    Task<string> UpdateSongAsync(Song song);
+    Task<string> UpdateSongAsync(SongUpdateModel song);
     Task DeleteSongAsync(int id);
     Task<List<Song>> GetSongsAsync();
     Task<Song> GetSongById(int id);
