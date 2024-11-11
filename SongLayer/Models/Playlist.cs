b68ee2f7a172cbb46e1571884ebf57b2afe054be
Nizami <nizamike229@mail.ru>
2024-11-11 15:50:49 +0500

    Added new models for playlists

---
+++ SongLayer/Models/Playlist.cs
@@ -0,0 +1,12 @@
+﻿namespace MyMusicApp.Models;
+
+public partial class Playlist
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string UserId { get; set; } = null!;
+
+    public virtual ICollection<Song> Songs { get; set; } = new List<Song>();
+}
---
+++ SongLayer/Models/PlaylistCreateModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MyMusicApp.Models;
+
+public class PlaylistCreateModel
+{
+    public required string UserId { get; set; }
+    public required string Name { get; set; }
+}
