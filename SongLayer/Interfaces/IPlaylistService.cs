7901441c64d14aec4f9567c91a4bd1ebd41402ba
Nizami <nizamike229@mail.ru>
2024-11-11 15:51:02 +0500

    Added interface to be implemented

---
+++ SongLayer/Interfaces/IPlaylistService.cs
@@ -0,0 +1,12 @@
+﻿using MyMusicApp.Models;
+
+namespace MyMusicApp.Interfaces;
+
+public interface IPlaylistService
+{
+    Task CreatePlaylistAsync(PlaylistCreateModel request);
+    Task DeletePlaylistAsync(int id);
+    Task AddSongToPlaylistAsync(int songId,int playlistId);
+    Task<List<Playlist>> GetAllPlaylistsAsync();
+    Task<List<Playlist>> GetPersonalPlaylistsAsync(string id);
+}
