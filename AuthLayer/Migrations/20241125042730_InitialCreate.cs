decf1e1f2ef1ee9db37f35cee58890a76c072801
Nizami <nizamike229@mail.ru>
2024-11-25 12:38:54 +0500

    Generated migrations

---
+++ AuthLayer/Migrations/20241125042730_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    username = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    password = table.Column<string>(type: "character varying(350)", maxLength: 350, nullable: false),
+                    logo = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("users_pk", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ SongLayer/Migrations/20241125042757_InitialCreate.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MyMusicApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "playlists",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    user_id = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("playlists_pk", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "songs",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    song_path = table.Column<string>(type: "character varying", nullable: false),
+                    cover_encoded = table.Column<string>(type: "character varying", nullable: false),
+                    user_id = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("songs_pk", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "playlist_songs",
+                columns: table => new
+                {
+                    playlist_id = table.Column<int>(type: "integer", nullable: false),
+                    song_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("playlist_songs_pkey", x => new { x.playlist_id, x.song_id });
+                    table.ForeignKey(
+                        name: "playlist_songs_playlist_id_fkey",
+                        column: x => x.playlist_id,
+                        principalTable: "playlists",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "playlist_songs_song_id_fkey",
+                        column: x => x.song_id,
+                        principalTable: "songs",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_playlist_songs_song_id",
+                table: "playlist_songs",
+                column: "song_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "playlist_songs");
+
+            migrationBuilder.DropTable(
+                name: "playlists");
+
+            migrationBuilder.DropTable(
+                name: "songs");
+        }
+    }
+}
