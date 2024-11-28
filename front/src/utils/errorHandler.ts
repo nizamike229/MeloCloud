bea789f7fb37f366d06947d07b8aa438df1d9a86
Nizami <nizamike229@mail.ru>
2024-11-28 18:41:29 +0500

    Removed useless comments

--- front/src/utils/errorHandler.ts
+++ front/src/utils/errorHandler.ts
@@ -1,9 +1,7 @@
 export const getErrorMessage = (error: any): string => {
     if (error.response) {
-        // Если есть ответ от сервера
         const { data } = error.response;
         
-        // Проверяем различные форматы ошибок
         if (typeof data === 'string') {
             return data;
         }
@@ -17,12 +15,10 @@ export const getErrorMessage = (error: any): string => {
         }
 
         if (data.errors) {
-            // Если есть массив ошибок валидации
             if (Array.isArray(data.errors)) {
                 return data.errors[0];
             }
             
-            // Если ошибки в объекте
             const firstError = Object.values(data.errors)[0];
             if (Array.isArray(firstError)) {
                 return firstError[0];
@@ -31,7 +27,6 @@ export const getErrorMessage = (error: any): string => {
         }
     }
     
-    // Если есть сообщение в самой ошибке
     if (error.message) {
         return error.message;
     }
